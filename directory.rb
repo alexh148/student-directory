591a2775dded1d5b08d792298b9ba3a2fb9d8aca
Alex Hutchings <alexhutchings4@gmail.com>
2018-12-20 17:27:48 +0000

    Exercises 1 - 4 completed. Needs some refactoring tomorrow

--- directory.rb
+++ directory.rb
@@ -1,8 +1,7 @@
 # All students in a student array
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names of the students\nTo finish, just hit return twice"
   # Create an empty array
   students = []
   # Get the first name
@@ -25,16 +24,35 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  accum = 0
+  while accum < students.length do
+    puts "#{accum+1}. #{students[accum][:name]} (#{students[accum][:cohort]} cohort)"
+    accum += 1
   end
+  # students.each_with_index do |student, index|
+  #   if student[:name].length < 12
+  #     puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  #   end
+  # end
 end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+def print_students_with_first_letter(students)
+  puts "Which letter would you like to use to filter students by name?"
+  letter = gets.chomp
+  students.select.with_index do |student, index|
+    if student[:name].chars.first == letter
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
 students = input_students
 print_header
+puts students[0][:name]
 print(students)
 print_footer(students)
+print_students_with_first_letter(students)
