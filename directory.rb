2ec87a8bc997af0194a9d134db505c08d1cf1a72
Alex Hutchings <alexhutchings4@gmail.com>
2018-12-27 11:37:53 +0000

    Refactored

--- directory.rb
+++ directory.rb
@@ -98,8 +98,7 @@ end
 def save_students
   file = File.open("students.csv", "w")
   @students.each do |student|
-    student_data = [student[:name], student[:cohort], student[:hobby]]
-    csv_line = student_data.join(",")
+    csv_line = [student[:name], student[:cohort], student[:hobby]].join(",")
     file.puts csv_line
   end
   file.close
